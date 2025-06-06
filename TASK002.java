9da776ea6ed0d487a980c93ffd8b1eeeb815a3fa
saikumarpentakota <saikumarpentakota9985@gmail.com>
2025-06-06 14:52:21 +0530

    TASK 009

---
+++ TASK002.java
@@ -0,0 +1,13 @@
+public class TASK002 {
+    public static void add(){
+        int a = 2;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("the sum of " + a + " and " + b + " is: " + sum);
+    }
+    public static void main(String[] args){
+        add();
+        add();
+        add();
+    }
+}
---
+++ TASK003.java
@@ -0,0 +1,9 @@
+
+public class TASK003 {
+    public static void main(String[] args)
+    {int a = 2;
+    int b = 5;
+    int sum = a + b;
+    System.out.println("the sum of "+a +" and "+b +" is: "+sum);
+    }
+}
