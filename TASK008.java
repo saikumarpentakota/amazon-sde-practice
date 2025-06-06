eeb2f4e7b74fd45f4178373eeb843d87f9f3b2b7
saikumarpentakota <saikumarpentakota9985@gmail.com>
2025-06-06 15:17:21 +0530

    TASK 009

---
+++ TASK008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TASK008{
+    public static void main(String[] args){
+        customer custom = new customer();
+        custom.accept();
+    }
+}
+class customer{
+    void accept(){
+        System.out.println("accept customer");
+    }
+}
---
+++ TASK11.java
@@ -0,0 +1,34 @@
+public class TASK11 {
+    public static void main(String[] args) {
+        int dayNumber = 3;
+
+        String dayName;
+        switch (dayNumber) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+        }
+
+        System.out.println("Today is " + dayName);
+    }
+}
