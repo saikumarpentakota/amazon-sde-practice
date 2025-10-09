2d29c1ad4476f21ce46b3df97fd0433bab6730e7
saikumarpentakota <Saikumarpentakota9985@gmail.com>
2025-10-09 16:12:37 +0530

    Delete Project-Employee_HRMS_ Portal_saipenta directory

--- Project-Employee_HRMS_ Portal_saipenta/src/main/java/org/example/Main.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("HRMS Application - Deploy to servlet container like Tomcat");
-    }
-}
--- Project-Employee_HRMS_ Portal_saipenta/src/main/java/org/example/util/DynamoDBConfig.java
+++
@@ -1,7 +0,0 @@
-package org.example.util;
-
-public class DynamoDBConfig {
-    public static final String DYNAMODB_HOST = "localhost";
-    public static final int DYNAMODB_PORT = 8000;
-    public static final String DYNAMODB_ENDPOINT = "http://" + DYNAMODB_HOST + ":" + DYNAMODB_PORT;
-}
